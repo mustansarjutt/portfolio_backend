c59c913cbd3435f87471590f74b0d0f189d97c3d
Mustansar Jutt <mhgjutt@gmail.com>
2025-04-01 17:14:02 +0500

    add rate limiter, helmet and update error handler

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from "express"
 import { ApiError } from "./utils/ApiError.js"
 import cors from "cors"
+import rateLimit from "express-rate-limit"
+import helmet from "helmet"
 
 const app = express()
+
+const limiter = rateLimit({
+    windowMs: 3 * 60 * 1000,
+    max: 2,
+    message: "Too many requests. Try again latter"
+})
+
+app.use(limiter)
+app.use(helmet())
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -28,10 +39,23 @@ app.use((err, req, res, next) => {
     if (err instanceof ApiError) {
         return res
         .status(err.statusCode)
-        .json(
-            {message: err.message, success: false}
-        )
+        .json({
+            message: err.message, 
+            success: false
+        })
     }
+    if (err.status === 429) {
+        return res
+        .status(429)
+        .json({
+            message: "Too many requests, please try again later", 
+            success: false
+        })
+    }
+    res.status(500).json({
+        message: "Internal Server Error", 
+        success: false
+    })
 })
 
 export { app }
